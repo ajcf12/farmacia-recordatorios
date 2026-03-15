288f5685b18a1dff30ec461ee8661ee7baaf780a
ajcf12 <ajcf12@github.com>
2026-03-14 21:21:25 -0400

    make-shortcut.ps1: remove GraphicsPath, use only basic shapes to avoid null errors

--- app/make-shortcut.ps1
+++ app/make-shortcut.ps1
@@ -1,68 +1,47 @@
 Add-Type -AssemblyName System.Drawing
 
-function RoundRect($path, $x, $y, $w, $h, $r) {
-    $path.AddArc($x, $y, $r, $r, 180, 90)
-    $path.AddArc($x + $w - $r, $y, $r, $r, 270, 90)
-    $path.AddArc($x + $w - $r, $y + $h - $r, $r, $r, 0, 90)
-    $path.AddArc($x, $y + $h - $r, $r, $r, 90, 90)
-    $path.CloseFigure()
-}
-
 $sz = 64
 $bmp = New-Object System.Drawing.Bitmap($sz, $sz)
 $g = [System.Drawing.Graphics]::FromImage($bmp)
 $g.SmoothingMode = [System.Drawing.Drawing2D.SmoothingMode]::AntiAlias
 $g.Clear([System.Drawing.Color]::Transparent)
 
-# --- Smartphone body ---
-$phonePath = New-Object System.Drawing.Drawing2D.GraphicsPath
-RoundRect $phonePath 6 4 30 50 6
-$phoneBrush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::FromArgb(30, 90, 180))
-$g.FillPath($phoneBrush, $phonePath)
+# Phone body (dark blue rectangle)
+$phoneBrush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::FromArgb(25, 85, 175))
+$g.FillRectangle($phoneBrush, 5, 3, 32, 52)
 
-# Screen
-$screenBrush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::FromArgb(180, 210, 255))
-$g.FillRectangle($screenBrush, 10, 12, 22, 32)
+# Screen (light blue)
+$screenBrush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::FromArgb(175, 210, 255))
+$g.FillRectangle($screenBrush, 9, 11, 24, 34)
 
-# Speaker slot (top)
-$slotBrush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::FromArgb(15, 60, 140))
-$g.FillRectangle($slotBrush, 16, 7, 10, 2)
+# Speaker (top center, dark)
+$darkBrush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::FromArgb(10, 50, 130))
+$g.FillRectangle($darkBrush, 16, 6, 10, 2)
 
-# Home button
-$g.FillEllipse($slotBrush, 17, 47, 8, 4)
+# Home button (bottom)
+$g.FillEllipse($darkBrush, 16, 48, 10, 5)
 
-# --- Pill (capsule) overlaid bottom-right ---
-# Shadow/outline
-$shadowBrush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::FromArgb(80, 0, 0, 0))
-$g.FillEllipse($shadowBrush, 34, 38, 28, 16)
-
-# Left half (white)
-$pillLeft = New-Object System.Drawing.Drawing2D.GraphicsPath
-$pillLeft.AddEllipse(33, 36, 28, 16)
+# Pill - left half (white)
 $whiteBrush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::White)
-$g.SetClip([System.Drawing.Rectangle]::FromLTRB(33, 36, 47, 53))
-$g.FillPath($whiteBrush, $pillLeft)
-$g.ResetClip()
+$g.FillEllipse($whiteBrush, 32, 36, 28, 16)
 
-# Right half (green)
-$pillRight = New-Object System.Drawing.Drawing2D.GraphicsPath
-$pillRight.AddEllipse(33, 36, 28, 16)
-$greenBrush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::FromArgb(40, 180, 100))
-$g.SetClip([System.Drawing.Rectangle]::FromLTRB(47, 36, 62, 53))
-$g.FillPath($greenBrush, $pillRight)
+# Pill - right half (green) using clip
+$greenBrush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::FromArgb(30, 175, 100))
+$clip = New-Object System.Drawing.Region([System.Drawing.Rectangle]::new(46, 36, 15, 17))
+$g.SetClip($clip, [System.Drawing.Drawing2D.CombineMode]::Replace)
+$g.FillEllipse($greenBrush, 32, 36, 28, 16)
 $g.ResetClip()
 
 # Pill outline
-$pillPen = New-Object System.Drawing.Pen([System.Drawing.Color]::FromArgb(180, 180, 180), 1)
-$g.DrawEllipse($pillPen, 33, 36, 28, 16)
+$pillPen = New-Object System.Drawing.Pen([System.Drawing.Color]::FromArgb(150, 150, 150), 1.5)
+$g.DrawEllipse($pillPen, 32, 36, 28, 16)
 
-# Divider line
-$divPen = New-Object System.Drawing.Pen([System.Drawing.Color]::FromArgb(180, 180, 180), 1)
-$g.DrawLine($divPen, 47, 37, 47, 51)
+# Pill divider
+$g.DrawLine($pillPen, 46, 37, 46, 51)
 
 $g.Dispose()
 
-# Save as .ico
+# Save icon
 $icoPath = "C:\FarmaciaApp\farmacia.ico"
 $ico = [System.Drawing.Icon]::FromHandle($bmp.GetHicon())
 $fs = New-Object System.IO.FileStream($icoPath, [System.IO.FileMode]::Create)
@@ -80,4 +59,4 @@ $sc.WorkingDirectory = "C:\FarmaciaApp"
 $sc.IconLocation = "$icoPath,0"
 $sc.Save()
 
-Write-Host "Acceso directo creado en el escritorio."
+Write-Host "Listo. Acceso directo creado en el escritorio."
